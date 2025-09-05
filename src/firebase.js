1d26c814a1a7dd01b820954df448ded129942af5
Hamic6 <dannyhamici@gmail.com>
2025-09-05 15:56:05 +0100

    mise à jour git

--- src/firebase.js
+++ src/firebase.js
@@ -7,20 +7,20 @@ import { getStorage } from "firebase/storage";
 
 // Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAoK8l-D1nEcjhmTSMOMeJovAlI0B8wCog",
-  authDomain: "dexbox-technologies.firebaseapp.com",
-  projectId: "dexbox-technologies",
-  storageBucket: "dexbox-technologies.appspot.com",
-  messagingSenderId: "871505208904",
-  appId: "1:871505208904:web:a8e1b30985849e7e765a7d",
-  measurementId: "G-25175PKG6E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-export const db = getFirestore(app); // <-- AJOUTE CETTE LIGNE
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export { app, analytics };
